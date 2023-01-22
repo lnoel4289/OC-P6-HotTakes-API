48479174c9bc3b6d97fb340d2e5dba189a33a95f
lnoel4289 <105934423+lnoel4289@users.noreply.github.com>
2023-01-22 10:46:56 +0100

    avancement app.js

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+app.post('/api/auth/signup', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Votre compte a bien été créé !'
+    });
+  });
+
+app.get('/api/sauces', (req, res, next) => {
+    console.log(req.body);
+    res.status(200).json({
+      message: 'Votre compte a bien été créé !'
+    });
+  });
+
+module.exports = app;
