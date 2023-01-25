90af904c17af9e3dbf10d614c36a1a71309bc06e
Laurent NOËL <lnoel4289@gmail.com>
2023-01-25 19:54:40 +0100

    DELETE route

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauce');
+const fs = require ('fs');
 
 exports.getAllSauces = (req, res, next) => {
   Sauce.find().then(
@@ -79,4 +80,21 @@ exports.modifySauce = (req, res, next) => {
     .catch(error => {
       res.status(400).json({ error });
     });
+};
+
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+  .then(sauce => {
+    if(sauce.userId != req.auth.userId) {
+      res.status(401).json({ message :'Non-autorisé !' });
+    } else {
+      const filename = sauce.imageUrl.split('/images')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+      });
+    };
+  })
+  .catch(error => res.status(500).json({ error }));
 };
