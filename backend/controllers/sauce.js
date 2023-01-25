aa22f59704b3f7d4b4763850ff7b8a990575dcad
Laurent NOËL <lnoel4289@gmail.com>
2023-01-25 19:12:34 +0100

    modifySauce route

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -57,3 +57,26 @@ exports.createSauce = (req, res, next) => {
     }
   );
 };
+
+exports.modifySauce = (req, res, next) => {
+  const sauceObject = req.file ? {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : {...req.body};
+
+  delete sauceObject._userId;
+
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(401).json({ message: 'Non-autorisé !' });
+      } else {
+        Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+        .catch(error => res.status(400).json({ error }));
+      };
+    })
+    .catch(error => {
+      res.status(400).json({ error });
+    });
+};
