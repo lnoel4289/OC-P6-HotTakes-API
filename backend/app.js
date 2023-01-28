d8ab53082a3846c5a221320800f3129551cca636
Laurent NOËL <lnoel4289@gmail.com>
2023-01-28 16:14:30 +0100

    save

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const path = require ('path');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_USER_MDP}${process.env.MONGO_DB_MARQUE}`,
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_USER_MDP}${process.env.MONGO_DB_MARK}`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
