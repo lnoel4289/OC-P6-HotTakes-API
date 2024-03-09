ac60df274cb331d242fcb2cc279f4cd077934e3b
Laurent NOËL <lnoel4289@gmail.com>
2024-03-09 17:18:53 +0100

    backend now online

--- backend/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dbConnect = () => mongoose.connect(process.env.DB_AUTH,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch((err) => console.log(`Connexion à MongoDB échouée ! ${err}`));
-
-  module.exports = dbConnect
